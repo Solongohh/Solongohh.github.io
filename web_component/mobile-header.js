84739a407e3f1a05f57641266007909614e64bf6
Solongohh <soko7596@gmail.com>
2023-05-28 17:15:20 +0800

    update

--- web_component/mobile-header.js
+++ web_component/mobile-header.js
@@ -1,13 +1,38 @@
 class MobileHeader extends HTMLElement {
     constructor() {
         super();
+        // this.attachShadow({mode:"open"})
         this.render();
     }
     render(){
         this.innerHTML = `
+        <style>
+        @import url('../css/root.css');
+
+        header{
+            display:flex;
+            justify-content:space-around;
+            align-items: center;
+        }
+        .mobileHeader{
+            width: 100%;
+            height: auto;
+            display: flex;
+            flex-direction: row;
+            align-items:center;
+        }
+        .mobileHeader img{
+            width: 35px;
+        }
+        nav ul{
+            margin-left: 20px;
+            margin-right: 20px;
+        }
+        </style>
         <nav class="mobileHeader">
             <ul><img src="../img/logo.png" alt="logo"></ul>
-            <ul><a href="index.html">COMSCOPE</a></ul>
+            <ul><a href="../pages/store.html">COMSCOPE</a></ul>
+            <ul><a href="../Buy section/Cart2.html" alt="shopping"><i class="fa-solid fa-cart-shopping"></i></a><shopping-cart></shopping-cart></ul>
         </nav>
         `
     }
@@ -28,5 +53,4 @@ class MobileHeader extends HTMLElement {
     }
 
 }
-
 window.customElements.define('mobile-header', MobileHeader);

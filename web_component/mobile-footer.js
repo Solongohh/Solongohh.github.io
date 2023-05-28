6d1718cb4b9ab49d47cd1a3e5b47d27678968a0f
Solongohh <soko7596@gmail.com>
2023-05-28 20:43:37 +0800

    responsive

--- web_component/mobile-footer.js
+++ web_component/mobile-footer.js
@@ -14,14 +14,6 @@ class MobileFooter extends HTMLElement {
             align-items: center;
             height: 50px;
         }
-        a img{
-            box-shadow:    0 0 .2rem #fff,
-            0 0 .2rem #fff,
-            0 0 2rem #bc13fe,
-            0 0 0.8rem #bc13fe,
-            0 0 2.8rem #bc13fe,
-            inset 0 0 1.3rem #bc13fe; 
-        }
         </style>
         <a href="../index.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/material-outlined/24/home--v2.png" alt="home--v2"/></a>
         <a href="../pages/console.html" alt="menu-icon"><img width="24" height="24" src="https://img.icons8.com/external-outline-agus-raharjo/64/external-playstation-electronic-outline-agus-raharjo.png" alt="external-playstation-electronic-outline-agus-raharjo"/></a>
